aac386ecec55a150c4fb98e8ba705a1d3bede750
hanypark <115128374+Selene-and-Luna@users.noreply.github.com>
2026-05-27 18:14:30 +0900

    Update linkedList.h

--- linkedList.h
+++ linkedList.h
@@ -1,6 +1,6 @@
 // linkedList.h
 
-typedef struct {
+typedef struct Node {
 	struct Node *prev;
 	struct Node *next;
 	int data;
@@ -9,7 +9,7 @@ typedef struct {
 Node *index_at(Node *head, int index);
 Node *end(Node *head);
 int size(Node *head);
-int insert_at(Node *head, int data, int index);
-int insert(Node *head, int data);
-int delete(Node *head, int index);
-int print(Node *head);
+void insert_at(Node *head, int data, int index);
+void insert(Node *head, int data);
+void delete(Node *head, int index);
+void print(Node *head);
